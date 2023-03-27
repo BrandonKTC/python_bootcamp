777d3bee6a9a02ed0982600adc6e9684b3419a9b
Brandon kwamou <brandonkwamou@gmail.com>
2023-03-27 13:56:54 +0200

    Package Creation

---
+++ Modules and Packages/myprogram.py
@@ -0,0 +1,6 @@
+from mymodule import my_func
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import mySubScript
+
+some_main_script.report_main()
+mySubScript.sub_report()
